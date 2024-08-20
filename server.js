da57219358079244c6eff431bcb462a0c976042d
Hafsa-Awan <123202172+Hafsa-Awan@users.noreply.github.com>
2024-08-20 12:42:36 +0500

    Update server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(cors());
 admin.initializeApp({
     credential: admin.credential.cert({
         type: process.env.FIREBASE_TYPE,
-        project_id: process.env.FIREBASE_PROJECT_ID,
-        private_key_id: leebi-83d05,
+        project_id: 'leebi-83d05',
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
         private_key: process.env.FIREBASE_PRIVATE_KEY,
         client_email: process.env.FIREBASE_CLIENT_EMAIL,
         client_id: process.env.FIREBASE_CLIENT_ID,
