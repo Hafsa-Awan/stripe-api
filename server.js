ff22cdddf318d737d36e81ee59b466ed2be02779
Hafsa-Awan <123202172+Hafsa-Awan@users.noreply.github.com>
2024-08-21 09:00:05 +0500

    Update server.js

--- server.js
+++ server.js
@@ -19,14 +19,14 @@ admin.initializeApp({
         project_id: 'leebi-83d05',
         private_key_id: process.env.PRIVATE_KEY_ID,
         private_key: process.env.PRIVATE_KEY,
-        client_email: process.env.FIREBASE_CLIENT_EMAIL,
-        client_id: process.env.FIREBASE_CLIENT_ID,
-        auth_uri: process.env.FIREBASE_AUTH_URI,
-        token_uri: process.env.FIREBASE_TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-        client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+        client_email: process.env.CLIENT_EMAIL,
+        client_id: process.env.CLIENT_ID,
+        auth_uri: process.env.AUTH_URI,
+        token_uri: process.env.TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.CLIENT_CERT_URL
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.env.DATABASE_URL
 });
 
 // Create a customer
